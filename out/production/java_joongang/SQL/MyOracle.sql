4c07f56787d8d2c0061b73db3b8521abeef07464
JunHyeok Seo <sjhgd107@naver.com>
2023-08-08 22:37:48 +0900

    Oracle DB 설치 및 IDEA와 연결

---
+++ out/production/java_joongang/SQL/MyOracle.sql
@@ -0,0 +1,17 @@
+-- 테이블 목록
+-- select * from tab;
+
+-- 레코드 검색
+select * from CUSTOMER order by NO asc ;
+
+-- 임시 테이블 삭제
+-- purge recyclebin;
+
+-- 예1.
+-- primary key(기본키) : 반드시 중복되지 않는 값을 저장 해야한다는 의미
+-- create table customer( no number(4)  primary key,
+--                        name varchar2(20),
+--                        email varchar2(20),
+--                        tel varchar2(20)  );
+
+--
